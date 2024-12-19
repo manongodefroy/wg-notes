7742ff17b11577f8eb55ff790d443d04591c2041
Manouchka <nuitdisme@gmail.com>
2024-12-19 10:27:34 +0000

    testing phase

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,20 @@
-import HelpScout, { NOTIFICATION_TYPES } from "@helpscout/javascript-sdk";
-import {
-  Button,
-  DefaultStyle,
-  Heading,
-  useSetAppHeight,
-  Text,
-  useHelpScoutContext,
-} from "@helpscout/ui-kit";
+import React from "react";
+import HelpScout from "@helpscout/javascript-sdk";
+import { DefaultStyle, Heading, useSetAppHeight } from "@helpscout/ui-kit";
 import { useEffect, useState } from "react";
 
 function App() {
   const appRef = useSetAppHeight();
-
-  const [userEmail, setUserEmail] = useState<string | undefined>(
-    "unknown user"
-  );
-
-  const [status, setStatus] = useState<string | undefined>("unknown status");
-
-  const { user, conversation } = useHelpScoutContext();
+  const [userFirstName, setUserFirstName] = useState("there");
   useEffect(() => {
-    setUserEmail(user?.email);
-    setStatus(conversation?.status);
-  }, [user, conversation]);
-
-  function onClick() {
-    HelpScout.showNotification(
-      NOTIFICATION_TYPES.SUCCESS,
-      "Hello from the sidebar app"
+    HelpScout.getApplicationContext().then(({ user }) =>
+      setUserFirstName(user?.firstName || "there")
     );
-  }
-
+  }, []);
   return (
     <div className="App" ref={appRef}>
       <DefaultStyle />
-      {/* <Heading level="h1">Hi, {userEmail}</Heading>
-      <Text>The conversation is {status}</Text>
-      <br /> */}
-      <Button size="sm" onClick={onClick}>
-        Click me
-      </Button>
+      <Heading level="h1">Hi {userFirstName}! 👋</Heading>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,18 @@ import { HelpScoutContextProvider } from "@helpscout/ui-kit";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <HelpScoutContextProvider>
-      <App />
+      <h4>
+        <a href="[URL here]">Jan's Famous Cupcakes</a>
+      </h4>
+      <p className="test">Customer since 05/07/2012 10:55 AM</p>
+      <ul>
+        <li>
+          <a href="[URL here]">Administrator</a>
+        </li>
+        <li>Business Plan</li>
+        <li>$1,245.00 lifetime value</li>
+      </ul>
+      {/* {<App />} */}
     </HelpScoutContextProvider>
   </React.StrictMode>
 );
